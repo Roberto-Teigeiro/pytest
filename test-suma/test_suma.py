be70a4dace7a62d67b285b82417d929b9c7d8456
Roberto-Teigeiro <A01643651@tec.mx>
2025-06-10 11:32:36 -0600

    Fix test

--- test-suma/test_suma.py
+++ test-suma/test_suma.py
@@ -5,6 +5,5 @@ from sumardosnumeros import sumar
 def test_sumar_correcto():
     assert sumar(2, 3) == 5
 
-# Esto fallará
 def test_sumar_falla():
-    assert sumar(2, 2) == 5
+    assert sumar(2, 2) == 4
