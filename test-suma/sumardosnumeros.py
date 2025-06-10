2857fedfabae154148b51e0a2f031e1fde678390
Roberto-Teigeiro <A01643651@tec.mx>
2025-06-10 11:19:55 -0600

    done

---
+++ test-suma/sumardosnumeros.py
@@ -0,0 +1,3 @@
+# archive de la app
+def sumar(a, b):
+    return a + b
---
+++ test-suma/testsuma.py
@@ -0,0 +1,10 @@
+# Archivo con las funciones de testing
+# Importamos la app sumar
+from sumardosnumeros import sumar
+
+def test_sumar_correcto():
+    assert sumar(2, 3) == 5
+
+# Esto fallará
+def test_sumar_falla():
+    assert sumar(2, 2) == 5
